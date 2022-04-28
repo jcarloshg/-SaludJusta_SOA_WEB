e6361df1ca1a427d9e68a053b9a164de35b4306e
jcarloshg <carlosj12336@gmail.com>
2022-04-28 18:21:42 -0500

    add function

--- src/pages/login/services/existAccount.js
+++ src/pages/login/services/existAccount.js
@@ -1,12 +1,41 @@
 
+const axios = require('axios').default;
 
 /**
  *
  * @param {string} email
  */
-export const existAccount = (email) => {
-    console.log(`[existAccount]`);
-    console.log(`[url] -> `, process.env.REACT_APP_URL);
-    console.log(`[API_APPOINTMENT] -> `, process.env.REACT_APP_API_APPOINTMENT);
+export const existAccount = async (email) => {
+
+    try {
+
+        const url = `${process.env.REACT_APP_URL}${process.env.REACT_APP_API_ACCOUNT}/existAccount`;
+
+        // const params = new URLSearchParams();
+        // params.append('asdf', 'value1');
+        // params.append('123', 'value2');
+
+        // const data = await axios.get(
+        //     url,
+        //     params
+        // );
+        // console.log(`[data] -> `, data);
+        const config = {
+            headers: {
+                'content-type': 'application/x-www-form-urlencoded',
+            },
+            data: {
+                "post_id": 1
+            },
+            params: {}
+        }
+        axios.get(url, config)
+
+    } catch (error) {
+        // console.log(`[error] -> `, error);
+    }
+
+    // console.log(`[url] -> `, process.env.REACT_APP_URL);
+    // console.log(`[API_APPOINTMENT] -> `, process.env.REACT_APP_API_APPOINTMENT);
 
 }
--- src/pages/login/useLogin.js
+++ src/pages/login/useLogin.js
@@ -4,8 +4,8 @@ import { existAccount } from "./services";
 
 export const useLogin = () => {
 
-    const iniciarSesion_proof = () => {
-        existAccount();
+    const iniciarSesion_proof = async () => {
+        await existAccount("alejando@email.com");
     }
 
     return {
