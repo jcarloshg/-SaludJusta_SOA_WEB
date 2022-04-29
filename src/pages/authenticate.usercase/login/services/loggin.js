6ef3c0ca413c7096e45a770471b9aef46b66b3cb
jcarloshg <carlosj12336@gmail.com>
2022-04-29 13:39:18 -0500

    CREATE service [loggin

--- src/pages/authenticate.usercase/login/services/loggin.js
+++ src/pages/authenticate.usercase/login/services/loggin.js
@@ -5,15 +5,17 @@ const axios = require('axios').default;
  *
  * @param {string} email
  */
-export const existAccount = async (email) => {
+export const loggin = async (email, password) => {
+
     try {
-        // http://localhost:8080/api_account/existAccount?email=alejando@email.com
+        // http://localhost:8080/api_account/loggin?email=carlosj12336@gmail.com&password=jose123
         const url = `${process.env.REACT_APP_URL}${process.env.REACT_APP_API_ACCOUNT}/existAccount`;
         const res = await axios.get(
             url,
             {
                 params: {
-                    email: email
+                    email: email,
+                    password: password
                 }
             }
         );
--- src/pages/authenticate.usercase/login/useLogin.js
+++ src/pages/authenticate.usercase/login/useLogin.js
@@ -1,27 +1,30 @@
 import createAdaptedUser from "../login/adapters/user.adapter";
 import { existAccount } from "./services";
+import { loggin } from "./services/loggin";
 
 
 export const useLogin = () => {
 
     const iniciarSesion_proof = async () => {
 
-        const emailExample = "alejando@email.com";
+        const emailExample = "carlosj12336@gmail.com";
+        const passwordExample = "jose123";
 
         const resExistAccount = await existAccount(emailExample);
-        const userClient = resExistAccount
-            ? createAdaptedUser(resExistAccount.data)
-            : null;
-
-        console.log(`[userClient] -> `, userClient);
 
         // * ================================
         // ! go to create new user
         // * ================================
-        if (userClient === null) {
+        if (resExistAccount.data === null) {
             // TODO - show error 
             return;
         }
+
+        const resLoggin = await loggin(emailExample, passwordExample);
+        const userClient = resExistAccount ? createAdaptedUser(resLoggin.data) : null;
+
+        console.log(`[userClient] -> `, userClient);
+
 
         // * ================================
         // ! go to the screens user
