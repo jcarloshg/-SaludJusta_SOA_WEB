92842825991be337e5dd76f484da041b526ab922
jcarloshg <carlosj12336@gmail.com>
2022-04-29 18:58:57 -0500

    ADD variable entorno

--- src/pages/record_exam_results.usecase/ExamenesPendientes/services/requestExamsToday.js
+++ src/pages/record_exam_results.usecase/ExamenesPendientes/services/requestExamsToday.js
@@ -1,21 +1,13 @@
 
-// const axios = require('axios').default;
+const axios = require('axios').default;
 
 export const requestExamsToday = async () => {
 
     try {
         // http://localhost:8080/api_account/existAccount?email=alejando@email.com
         const url = `${process.env.REACT_APP_URL}${process.env.REACT_APP_API_EXAM}/requestExamsToday`;
-        return url;
-        // const res = await axios.get(
-        //     url,
-        //     {
-        //         params: {
-        //             email: email
-        //         }
-        //     }
-        // );
-        // return res.data;
+        const res = await axios.get(url);
+        return res.data;
     } catch (error) {
         console.log(`[error] -> `, error);
         return null;
--- src/pages/record_exam_results.usecase/ExamenesPendientes/useExamenesPendientes.js
+++ src/pages/record_exam_results.usecase/ExamenesPendientes/useExamenesPendientes.js
@@ -1,10 +1,15 @@
 import { useEffect } from "react"
+import { requestExamsToday } from "./services/requestExamsToday";
 
 export const useExamenesPendientes = () => {
 
     useEffect(() => {
 
         console.log(`[useExamenesPendientes] -> useEffect`);
+        (async () => {
+            const data = await requestExamsToday();
+            console.log(`[data] -> `, data);
+        })();
 
         return () => { }
     }, [])
