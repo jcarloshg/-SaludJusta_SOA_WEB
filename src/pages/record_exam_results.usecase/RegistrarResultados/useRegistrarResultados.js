7b347098b48cbd5d89c73829dbae4d6d95fe29e5
jcarloshg <carlosj12336@gmail.com>
2022-05-02 00:58:06 -0500

    {

--- src/pages/record_exam_results.usecase/RegistrarResultados/useRegistrarResultados.js
+++ src/pages/record_exam_results.usecase/RegistrarResultados/useRegistrarResultados.js
@@ -1,3 +1,4 @@
+import { useInput } from '@nextui-org/react';
 import React, { useContext, useEffect, useState } from 'react'
 import User from '../../../models/User.entitie';
 import { DataContext } from '../HomeExam/contexts/DataContext';
@@ -8,13 +9,27 @@ export const useRegistrarResultados = () => {
 
     const [user, setUser] = useState(new User({}));
 
+    const {
+        value: controlledValue, setValue: setControlledValue, reset, bindings,
+    } = useInput("");
+
     useEffect(() => {
         // ! if not exist this condition the progam die D:
         if (data.idUser === null) return;
         setUser(new User(data));
     }, []);
 
+    const saveResults = () => {
+        console.log(controlledValue);
+    }
+
+    const cancel = () => {
+        console.log(controlledValue);
+    }
+
     return {
         user,
+        bindings,
+        saveResults
     };
 }
